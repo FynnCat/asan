8bdc774101a8de0f9976831b68c37d09e4142715
FynnCat <Fynnhb@icloud.com>
2023-05-02 14:40:38 +1000

    Midday Tuesday, bugfixing & starting to code the end

--- asanProject/Assets/Scripts/ExperienceManager.cs
+++ asanProject/Assets/Scripts/ExperienceManager.cs
@@ -1,16 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
-
-public class ExperienceManager : MonoBehaviour
-{
-    public int exp, ;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-=======
 using UnityEngine.UI;
 
 public class ExperienceManager : MonoBehaviour
@@ -23,7 +12,7 @@ public class ExperienceManager : MonoBehaviour
         HabBadSlider.maxValue = 30;
         HabGoodSlider.maxValue = 30;
         StartCoroutine(WaitTimne());
->>>>>>> Stashed changes
+
     }
 
     // Update is called once per frame
@@ -31,13 +20,12 @@ public class ExperienceManager : MonoBehaviour
     {
         
     }
-<<<<<<< Updated upstream
-=======
+
     IEnumerator WaitTimne()
     {
         yield return new WaitForSeconds(5);
         exp = exp + 10;
         StartCoroutine(WaitTimne());
     }
->>>>>>> Stashed changes
+
 }
