5803bcacf2905149c392c51227ea9f9b630c9ee5
AIE\s213246 <yeatleson@gmail.com>
2023-05-09 10:34:48 +1000

    Main menu complete - xs

--- asanProject/Assets/Resolution.cs
+++ asanProject/Assets/Resolution.cs
@@ -8,7 +8,7 @@ public class Resolution : MonoBehaviour
     [SerializeField] private TMP_Dropdown resolutionD;
 
     private UnityEngine.Resolution[] resolutions;
-    private List<Resolution> filteredResolutions;
+    private List<UnityEngine.Resolution> filteredResolutions;
 
     private float currentRefreshRate;
     private int currentResolutionIndex = 0;
@@ -18,18 +18,40 @@ public class Resolution : MonoBehaviour
     {
         UnityEngine.Resolution[] resolutions1 = Screen.resolutions;
         resolutions = resolutions1;
-        filteredResolutions = new List<Resolution>();
+        filteredResolutions = new List<UnityEngine.Resolution>();
 
         resolutionD.ClearOptions();
         currentRefreshRate = Screen.currentResolution.refreshRate;
 
-        for (int i = 0, i < resolutions.Length; i++)
+        for (int i = 0; i < resolutions.Length; i++)
         { 
             if (resolutions[i].refreshRate == currentRefreshRate)
             {
-                filteredResolutions.Add(resolutions[i]);
+                filteredResolutions.Add(resolutions1[i]);
             }
         }
 
+        List<string> options = new List<string>();
+        for (int i = 0; i < filteredResolutions.Count; i++)
+        {
+            string resolutionOption = filteredResolutions[i].width + "x" + filteredResolutions[i].height + " " + filteredResolutions[i].refreshRate + " Hz";
+            options.Add(resolutionOption);
+            if (filteredResolutions[i].width == Screen.width && filteredResolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionD.AddOptions(options);
+        resolutionD.value = currentResolutionIndex;
+        resolutionD.RefreshShownValue();
+
     }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        UnityEngine.Resolution resolution = filteredResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+
 }
