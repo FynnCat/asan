1cb31c5d8d4bb92e1a9c92a05b9eee958c51619c
FynnCat <Fynnhb@icloud.com>
2023-05-09 10:32:20 +1000

    Update SaveData.cs

--- asanProject/Assets/Scripts/SaveData.cs
+++ asanProject/Assets/Scripts/SaveData.cs
@@ -11,6 +11,7 @@ public class PlayerData
     public float conpletion;
     public float levelAmount;
     public int levelAmountCompleted;
+    public float playerChoice;
 
     public PlayerData(SaveManager _saveManager)
     {
@@ -18,6 +19,7 @@ public class PlayerData
         time = _saveManager.time;
         level = _saveManager.level;
         levelAmountCompleted = _saveManager.levelAmountCompleted;
+        playerChoice = _saveManager.playerChoice;
 
 
     }
